ccaf0ab48722f8a54d24a010e28b4cdb3f089e56
IfeoluwaAroboto <104203257+IfeoluwaAroboto@users.noreply.github.com>
2022-06-22 21:46:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,5 @@
-var A,B,C, operator;
-A = prompt("Enter a number here (0-9)");
-operator = prompt ("Enter an Arithmetic Opeartor(+,-,*,/)")
-B = prompt("Enter a number here (0-9)")
+let text = document.querySelector(".personal-details")
+text.style.color = "hotpink" 
 
-function Maths(){
-if (operator =="+"){
- C =  Number(A) + Number(B)
-}
-    else if (operator =="*" ) {
-        C = Number(A) * Number(B)
-    } 
-    else if (operator =="/" ) {
-        C = Number(A) / Number(B)
-    } 
-    else if( operator== "-") {
-        C = Number(A) - Number(B)
-    }
-    else if (operator = !null ){
-        C =  "Error!"
-    }
-    else if (A = !null ){
-        C = "Error!"
-    }
-    else if (B = !null ){
-        C = "Error!"
-    }
-
-    
-    else{
-        
-    }
-
-
-return C
-    
-}
-alert(Maths()) 
+let intro = "My name is Aroboto Ife<br> My height is 5'5 ft <br> I am Nigerian"
+text.innerHTML = intro 
