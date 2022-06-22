b17f68a79166b786b8607ac64fe8c412019088e4
IfeoluwaAroboto <104203257+IfeoluwaAroboto@users.noreply.github.com>
2022-06-22 21:58:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 let text = document.querySelector(".personal-details")
-text.style.color = "hotpink" 
-
-let intro = "My name is Aroboto Ife<br> My height is 5'5 ft <br> I am Nigerian"
-text.innerHTML = intro 
+text.style.color = "hotpink"
+let intro = "<h2> My name is Aroboto Ife </h2> <h3> My height is 5'6 ft <br> I am Nigerian </h3>"
+ text.innerHTML = intro
